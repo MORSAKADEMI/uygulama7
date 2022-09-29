528b2155c04a3912fb14251ab39c1ad219dae33c
Burak Üstün <79836033+burakbfm@users.noreply.github.com>
2022-09-29 18:33:41 +0300

    Create 7-dictionary-methods-demo.py

---
+++ 7-dictionary-methods-demo.py
@@ -0,0 +1,23 @@
+'''
+    player 1: 
+        id           => 1
+        name         => Cristiano Ronaldo
+        yearOfBirth  => 1985
+        nationality  => Portugal
+        current_team => Portugal
+        history      => Juventus,Real Madrid,Portugal
+
+    player 2: 
+        id           => 2
+        name         => Lionel Messi
+        yearOfBirth  => 1987
+        nationality  => Argentina
+        current_team => Barcelona,
+        history      => Barcelona,Argentina,Portugal
+'''
+
+# 1- Yukarıda verilen bilgileri liste içerisinde saklayınız.
+
+# 2- id' e göre arama yapınız.
+
+# 3- id' e göre bilgi kayıt siliniz.
